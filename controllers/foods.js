8e2b3a5100cefdf0c6bd30022be519e81e1bc4c0
GenioZx <eli.rodri14@email.com>
2025-07-25 16:38:54 -0400

    created delete form and button, created delete route

--- controllers/foods.js
+++ controllers/foods.js
@@ -37,3 +37,15 @@ router.post('/', async (req, res) => {
     res.redirect('/');
   }
 });
+
+router.delete('/:foodId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.pantry.id(req.params.foodId).deleteOne();
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+})
