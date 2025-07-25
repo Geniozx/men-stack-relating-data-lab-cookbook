3ebfe0e6fb78152939f92536ccc66ae7244a575e
GenioZx <eli.rodri14@email.com>
2025-07-25 16:24:00 -0400

    pantry items added to the index page

--- controllers/foods.js
+++ controllers/foods.js
@@ -26,3 +26,14 @@ router.get('/new', async (req, res) => {
   res.render('foods/new.ejs');
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.pantry.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
